7758013ea14eb7926c35ca6bb2a04df4aee93292
robloxtools <olipuka69@gmail.com>
2026-08-31 16:09:45 +0200

    server.js

--- server.js
+++ server.js
@@ -3,7 +3,10 @@ const { MongoClient } = require("mongodb");
 require("dotenv").config();
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
+});
 
 app.use(express.json({ limit: "5mb" }));
 app.use(express.static("public"));
